3ca0340bba9b3236b94fe886f3d9dd3ac31199fc
Nugroho Dimas <nugroho.dimas@inmotion.co.id>
2022-10-24 17:14:17 +0700

    home section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,32 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-menu'),
+      navToogle = document.getElementById('nav-toogle'),
+      navClose = document.getElementById('nav-close')  
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToogle){
+    navToogle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose){
+    navClose.addEventListener('click', ()=> {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
